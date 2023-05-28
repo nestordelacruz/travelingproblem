9ef3fd8361f5748e0b057a20831a40385605532b
Marco Velez <marcovelezdc@gmail.com>
2023-05-27 22:48:25 -0700

    Added Neighboor Heuristic

--- main.py
+++ main.py
@@ -30,6 +30,49 @@ def graph(vertices):
     return graph
 
 
-# Ejemplo de generacion de un grafo simetrico y que sigue la propiedad de Triangle Inequality
-generated_graph = graph(5)
+generated_graph = graph(10)
 print(generated_graph)
+
+def nearest_neighboor(graph):
+    # Iniciamos en cualquier nodo, en este caso supongamos el primero del grafo generado
+    startingPoint = 0
+
+    # Recorrido encontrado (considerando que el punto de partida es el inicial)
+    path = [startingPoint]
+
+    counter = 1
+    # Crear una lista de nodos que pueden ser visitados
+    nodes = list(range(len(graph[0])))
+    # Variable de costo que se actualiza con cada iteracion para determinar el costo total de la ruta encontrada
+    totalCost = 0
+    while(counter < len(graph[0])):
+        # Excluir todos los nodos visitados y el nodo raiz
+        nodes = [x for x in nodes if x != startingPoint]
+
+        # Agarrar el valor maximo como el minimo de forma inicial
+        min = max(graph[startingPoint])
+
+        # Identificar las posibles ciudades por traversar considerando que ya visitamos ciertas ciudades, que no debe
+        # de visitarse a si mismo y que se quiere visitar al nodo con menor costo
+        for index, possible_city in enumerate(graph[startingPoint]):
+            # Si la ciudad no ha sido recorrida entonces es candidato
+            if index in nodes:
+                # Si el costo encontrado es menor guardar el indice para descartar la busqueda de dicho nodo para traversos futuros
+                if possible_city <= min:
+                    min = possible_city
+                    indexPosition = index
+
+        # Actualizar punto de partida a que ahora sea con el indice correspondiente al del valor minimo (como si fuera un recorrido)
+        startingPoint = indexPosition
+        # Agregar el costo de la ruta encontrada
+        totalCost += min
+
+        # Agregar la ruta de partida encontrada
+        path.append(startingPoint)
+        counter += 1
+
+    print("------ NEAREST NEIGHBOOR HEURISTIC ------")
+    print("CAMINO ENCONTRADO: ", path)
+    print("COSTO TOTAL DE LA RUTA: ", totalCost)
+
+nearest_neighboor(generated_graph)
